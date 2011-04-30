d9945fd1b1d6ecc14c6890f99449a4d58cb7431a
cohesioN241 <cohesioN241@gmail.com>
2011-04-30 16:55:49 -0500

    GUI overhaul

--- form.py
+++ form.py
@@ -1,67 +1,79 @@
 # a reusable form class, used by getfilegui (and others)
 
 from Tkinter import *
+import Image, ImageTk
 entrysize = 40
 
-class Form:                                           # add non-modal form box
-    def __init__(self, labels, modes, parent=None):          # pass field labels list
-        box = Frame(parent)
-        box.pack(expand=YES, fill=X)
-        rows = Frame(box, bd=2, relief=GROOVE)        # box has rows, button
-        lcol = Frame(rows)                            # rows has lcol, rcol
-        rcol = Frame(rows)                            # button or return key,
-        rows.pack(side=TOP, expand=Y, fill=X)         # runs onSubmit method
-        lcol.pack(side=LEFT)
-        rcol.pack(side=RIGHT, expand=Y, fill=X)
+class FormTest:                                           # add non-modal form box
+    def __init__(self, labels, parent=None):          # pass field labels list
+	#box = Frame(parent)
         self.content = {}
+	Label(text='Metagui', font=("Helvetica", 24)).grid(row=0, column=1, sticky=W+E)
+	Label(text='', width=7).grid(row=0, column=2)
+	r = 1
+	self.filetype = StringVar()
+	self.engine = StringVar()
         for label in labels:
-        	Label(lcol, text=label).pack(side=TOP)
-        	entry = Entry(rcol, width=entrysize)
-        	entry.pack(side=TOP, expand=YES, fill=X)
-        	self.content[label] = entry
-	self.proxy = IntVar()
-	Checkbutton(box, text='Use Proxy', variable=self.proxy).pack(side=LEFT, anchor=W)
-	box2 = Frame(parent)
-	box2.pack(expand=YES, fill=X)
-	Label(box2, text='Engine').pack(side=LEFT)
-	self.v = IntVar()
-	for mode, value in modes:
-		rad = Radiobutton(box2, text=mode, variable=self.v, value=value).pack(side=LEFT, anchor=W)
-	box3 = Frame(parent)
-	box3.pack(expand=YES, fill=X)
-        Button(box3, text='Cancel', command=self.onCancel).pack(side=RIGHT)
-        Button(box3, text='Submit', command=self.onSubmit).pack(side=RIGHT)
-        box.master.bind('<Return>', (lambda event, self=self: self.onSubmit()))
+        	Label(text=label, width=7).grid(row=r, column=0)
+		if label=='Filetype':
+			self.filetype.set('all')
+			OptionMenu(parent, self.filetype, 'all','pdf','doc','xls','ppt','sdw','mdb','sdc','odp','ods','docx','xlsx','pptx', 'zip').grid(row=r, column=1, sticky=W)
+		elif label=='Engine':
+			self.engine.set('Google')
+			OptionMenu(parent, self.engine, 'Google', 'Bing', 'Yahoo!', 'ACK').grid(row=r, column=1, sticky=W)
+		else:
+        		entry = Entry(width=entrysize)
+			entry.grid(row=r, column=1, sticky=W)
+        		self.content[label] = entry
+		r = r+1
+		if label=='Domain':
+			Label(text='example: microsoft.com').grid(row=r, column=1, sticky=W)
+		elif label=='Limit':
+			Label(text='# of files you wish to retrieve').grid(row=r, column=1, sticky=W)
+		elif label=='Filetype':
+			Label(text='filetype you wish to search for').grid(row=r, column=1, sticky=W)
+		elif label=='Engine':
+			Label(text='search engine you wish to use').grid(row=r, column=1, sticky=W)
+		elif label=='Output':
+			Label(text='example: microsoft.html').grid(row=r, column=1, sticky=W)
+		elif label=='LocalDir':
+			Label(text='example: microsoft-files').grid(row=r, column=1, sticky=W)
+		elif label=='Proxy':
+			Label(text='example: 75.64.56.207:41277').grid(row=r, column=1, sticky=W)
+		elif label=='Level':
+			Label(text='for ACK only. specify depth of search').grid(row=r, column=1, sticky=W)
+		r = r+1
+	Button(text='Cancel', command=self.onCancel).grid(row=r, column=1, sticky=E)
+	Button(text='Submit', command=self.onSubmit).grid(row=r, column=2, sticky=E)
+        #box.master.bind('<Return>', (lambda event, self=self: self.onSubmit()))
 
     def whichEngine(self):
-	return self.v.get()
+	return self.engine.get()
 
-    def useProxy(self):
-	return self.proxy.get()
+    def whatFiletype(self):
+	return self.filetype.get()
 
     def onSubmit(self):                                      # override this
         for key in self.content.keys():                      # user inputs in 
             print key, '\t=>\t', self.content[key].get()     # self.content[k]
-	#self.progressbar()
-	#self.update_progress()
 
     def onCancel(self):                                      # override if need
         Tk().quit()                                          # default is exit
 
-class DynamicForm(Form):
+class DynamicForm(FormTest):
     def __init__(self, labels=None):
         import string
         labels = string.split(raw_input('Enter field names: '))
-        Form.__init__(self, labels)
+        FormTest.__init__(self, labels)
     def onSubmit(self):
         print 'Field values...'
-        Form.onSubmit(self)           
+        FormTest.onSubmit(self)           
         self.onCancel()              
         
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 1:
-        Form(['Name', 'Age', 'Job'])     # precoded fields, stay after submit
+        FormTest(['Name', 'Age', 'Job'])     # precoded fields, stay after submit
     else:
         DynamicForm()                    # input fields, go away after submit
     mainloop()    
--- metagui.py
+++ metagui.py
@@ -1,20 +1,18 @@
-#!/usr/bin/python
 from Tkinter import *
 from tkMessageBox import showinfo
 import os, sys, thread
-from form import *
+from formtest import *
 import metagoofil
 #from widget import *
 import ackSearcher
 
-class MetaForm(Form):
+class MetaForm(FormTest):
 	def __init__(self):
 		root = Tk()
 		root.title(self.title)
-		labels = ['Domain', 'Limit', 'Filetype',
+		labels = ['Domain', 'Limit', 'Filetype', 'Engine',
 					'Output', 'LocalDir', 'Proxy', 'Level']
-		modes = [('Google', 1), ('Bing', 2), ('Yahoo!', 3), ('ACK', 4)]
-		Form.__init__(self, labels, modes, root)
+		FormTest.__init__(self, labels, root)
 		self.mutex = thread.allocate_lock()
 		self.threads = 0
 	def metatest(self, argv):
@@ -24,28 +22,26 @@ class MetaForm(Form):
 		self.threads = self.threads - 1
 		self.mutex.release()
 	def onSubmit(self):
-		Form.onSubmit(self)
+		FormTest.onSubmit(self)
 		domain = self.content['Domain'].get()
 		limit = self.content['Limit'].get()
-		filetype = self.content['Filetype'].get()
+		#filetype = self.content['Filetype'].get()
+		filetype = self.whatFiletype()
 		output = self.content['Output'].get()
 		localdir = self.content['LocalDir'].get()
 		level = self.content['Level'].get()
 		#engine = self.content['Engine'].get()
-		proxy = ''
-		if self.useProxy() == 1:
-			proxy = self.content['Proxy'].get()
-			print "Using proxy: " + proxy
+		proxy = self.content['Proxy'].get()
 		engine = self.whichEngine()
-		if engine == 1:
+		if engine == 'Google':
 			engine = 'google.com'
-		elif engine == 2:
+		elif engine == 'Bing':
 			engine = 'bing.com'
-		elif engine == 3:
+		elif engine == 'Yahoo!':
 			engine = 'yahoo.com'
-		elif engine == 4:
+		elif engine == 'ACK':
 			engine = 'ACK'
-			ack = 1
+			#ack = 1
 		print 'Engine\t=>\t', engine
 
 		self.mutex.acquire()
@@ -63,32 +59,9 @@ class MetaForm(Form):
 			else: p = {'http':'http://'+proxy}
 			localdir='./' + localdir + '/'
 			self.runAck(domain, filetype, limit, level, './robots.txt', 3, p, localdir)
-#		self.progressbar()
-#		self.update_progress()
 	
 	def runAck(self, *args):
 		self.acktest(*args)
-		
-		
-	def progressbar(self):
-		t = Tk()
-		self.progval = IntVar(t)
-		progmsg = StringVar(t); progmsg.set("Compute in progress...")
-		b = Button(t, relief=LINK, text="Quit (using bwidget)", command=t.destroy)
-		b.pack()
-		t.withdraw()
-		self.c = ProgressDialog(t, title="Please wait...",
-		                   type="infinite",
-		                   width=20,
-		                   stop="Stop",
-		                   textvariable=progmsg,
-		                   variable=self.progval,
-		                   command=lambda: self.c.destroy()
-		                   )
-	
-	def update_progress(self):
-		self.progval.set(2)
-		self.c.after(20, self.update_progress)
 		
 	def onCancel(self):
 		if self.threads == 0:
@@ -97,7 +70,7 @@ class MetaForm(Form):
 			showinfo(self.title, 'Cannot exit: %d threads running' % self.threads)
 
 class MetaGetForm(MetaForm):
-	title = 'Hi 141'
+	title = 'Metagui'
 	def test(self, argv):
 		metagoofil.test(argv)
 	def acktest(self, *args):
